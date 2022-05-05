3838b50ef00329c65eda2f0c2f031da2d400b091
keeddn <104479928+keeddn@users.noreply.github.com>
2022-05-05 19:58:31 +0800

    Add files via upload

---
+++ shiyan8/js/8-1.js
@@ -0,0 +1,20 @@
+$(init)
+function init() {
+    $('.g-cnt').click(function (e) {
+        let t=e.currentTarget;
+        let tmp=$(t).clone();
+        e.stopPropagation();
+        $(tmp).css("width","190px");
+        $(tmp).css("height","110px");
+        $(tmp).css("padding-top","70px");
+        $('.bigImage').empty();
+        $('.bigImage').css("display","flex");
+        $('.bigImage').append($(tmp).clone());
+        
+    })
+    $('body').click(function () {
+        $('.bigImage').empty();
+        $('.bigImage').css("display","none");
+    })
+
+}
